b857f471dfa56215098c5385dcd1e98de1e4059e
Aashish AIilawadi <aashishailawadi@gmail.com>
2018-07-23 00:41:57 +0530

    Added basic table structure

--- views/script.js
+++ views/script.js
@@ -10,15 +10,65 @@ function getData() {
     xmlHttp.onreadystatechange = function(){
         if(this.readyState == 4){
             if (this.status == 200){
-
+                addTables(JSON.parse(this.response));
             }
             else if( this.status == 500){
-
+                document.getElementById('waiting').innerHTML = "Some error occured";
             }
         }
-    }
+    };
 
     xmlHttp.open('GET', url, true);
     xmlHttp.setRequestHeader('Content-Type', mimeType);
     xmlHttp.send(null);
+}
+
+function addTables(data){
+    addWaitingData(data.waiting);
+    addOngoingData(data.ongoing);
+    addCompletedData(data.completed);
+}
+
+function addWaitingData(waiting){
+    var table = document.getElementById('waiting');
+    table.border = '1';
+    var tr = document.createElement('TR');
+    table.appendChild(tr);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode('Waiting'));
+    th.width = '150';
+    tr.appendChild(th);
+    for(var i = 0 ; i < waiting.length ; i++){
+
+    }
+}
+
+function addOngoingData(ongoing){
+    var table = document.getElementById('ongoing');
+    table.border = '1';
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+    var tr = document.createElement('TR');
+    tableBody.appendChild(tr);
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode('Ongoing'));
+    th.width = '150';
+    tr.appendChild(th);
+    for(var i = 0 ; i < ongoing.length ; i++){
+
+    }
+}
+
+function addCompletedData(completed){
+    var table = document.getElementById('completed');
+    table.border = '1';
+    var tr = document.createElement('TR');
+    table.appendChild(tr);
+    var th = document.createElement('TH');
+    th.width = '150';
+    th.appendChild(document.createTextNode('Completed'));
+    tr.appendChild(th);
+    for(var i = 0 ; i < completed.length ; i++){
+
+    }
 }
